75f9d9d878c6d253eb8472d7da378d32c880d1f3
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-10-28 14:04:38 +0000

    Fixed non-carnonical redirect

--- global.php
+++ global.php
@@ -4,7 +4,7 @@
 
 	if (substr($Request['path'], -4, 4) == '.php') {
 		header ('HTTP/1.1 301 Moved Permanently');
-		header ('Location: http://eustasy.org'.$Canonical);
+		header ('Location: http://howtoelementaryos.org'.$Canonical);
 	}
 
 ?>
