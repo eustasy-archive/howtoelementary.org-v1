0d5c97e5f71bbea63731c0a72a339d22ede1519f
Lewis Goddard <lewisgoddard@eustasy.org>
2014-07-11 13:08:47 +0100

    Update global.php

--- global.php
+++ global.php
@@ -3,8 +3,7 @@
 	$Request = parse_url($_SERVER['REQUEST_URI']);
 
 	if (substr($Request['path'], -4, 4) == '.php') {
-		header ('HTTP/1.1 301 Moved Permanently');
-		header ('Location: http://howtoelementaryos.org'.$Canonical);
+		header('HTTP/1.1 301 Moved Permanently');
+		header('Location: http://howtoelementaryos.org'.$Canonical);
+		exit();
 	}
-
-?>
