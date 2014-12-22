30afe7ea6339aca1def57f29a7b7c1d5d15d76b8
Lewis Goddard <lewisgoddard@eustasy.org>
2014-12-22 10:53:45 +0000

    Update header.php

--- header.php
+++ header.php
@@ -31,6 +31,7 @@
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-45667989-4', 'auto');
+		ga('require', 'displayfeatures');
 		ga('send', 'pageview');
 	</script>
 
